1eb196b076778e63c8edef047e4fe560e68998a9
Shakthi Prasad G S <shakthi.gs87@gmail.com>
2015-07-19 22:23:45 +0530

    Welcome to actor.

--- Assets/Plugins/UnityActions/Scripts/TestCases/TestActions.cs
+++ Assets/Plugins/UnityActions/Scripts/TestCases/TestActions.cs
@@ -6,6 +6,14 @@ public class TestActions : MonoBehaviour
 {
 	public  Transform movingObject;
 
+
+	IEnumerator anANimtion()
+	{
+		var  anActor = new Actor(movingObject);
+		yield return anActor.MoveBy(10,new Vector3(10,10,10));
+		yield return anActor.MoveTo(4,new Vector3(10,10,10));
+	}
+
 	void Start ()
 	{
 //		Action.Run(movingObject,new MoveBy(10 ,new Vector3(10,1,1)));
@@ -13,9 +21,9 @@ public class TestActions : MonoBehaviour
 //
 
 
-		Action.Run(movingObject,new Spawn(new MoveBy(10 ,new Vector3(10,1,1)),new MoveBy(10,new Vector3(0,30,0)),new RotateBy(10,new Vector3(0,180,0))));
-		//		Action.Run(movingObject,new RotateBy(5,new Vector3(10,0,0)));
-		//
+//		Action.Run(movingObject,new Spawn(new MoveBy(10 ,new Vector3(10,1,1)),new MoveBy(10,new Vector3(0,30,0)),new RotateBy(10,new Vector3(0,180,0))));
+//		//		Action.Run(movingObject,new RotateBy(5,new Vector3(10,0,0)));
+//		//
 
 //		Sequence q= new Sequence(new MoveBy(3 ,new Vector3(10,1,1)),
 //		                                     new RotateBy(5,new Vector3(180,0,0)),
@@ -32,6 +40,10 @@ public class TestActions : MonoBehaviour
 
 		//Action.Run(movingObject,new RepeatForever(new RotateBy(0.5f,new Vector3(10,0,0))));
 
+
+
+
+		StartCoroutine(anANimtion);
 
 	}
 
