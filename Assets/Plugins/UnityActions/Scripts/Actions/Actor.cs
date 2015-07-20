cfc45e00e542ef6f93d05ec22bfb6334a3f304ee
Shakthi Prasad G S <shakthi.gs87@gmail.com>
2015-07-20 07:04:15 +0530

    Added implemented action actions
    (Code is failed to work)

---
+++ Assets/Plugins/UnityActions/Scripts/Actions/Actor.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+namespace CC
+{
+
+public class Actor  {
+
+	 Transform transform;
+	 FiniteTimeAction action;
+
+	public Actor(Transform transform)
+	{
+		this.transform=transform;
+	}
+	
+
+	public IEnumerator RunAction(FiniteTimeAction anAction)
+	{
+			if(action !=null)
+				Debug.LogError("An action is already running");
+			else
+			{
+				action = anAction;
+			
+				Action.Run(transform,action);
+				yield return new WaitForSeconds(action.GetDuration());
+				while(!action.IsDone())
+				{
+					yield return null;
+				}
+				action=null;
+			}
+
+	}
+
+	public IEnumerator MoveBy(float duraction,Vector3 diff)
+	{
+			Debug.Log("Comming here");
+
+			yield return RunAction(new CC.MoveBy(duraction,diff));
+	}
+
+	public IEnumerator MoveTo(float duraction,Vector3 destination)
+	{
+			Debug.Log("Comming here");
+			yield return RunAction(new CC.MoveTo(duraction,destination));
+	}
+
+
+}
+}
--- Assets/Plugins/UnityActions/Scripts/TestCases/TestActions.cs
+++ Assets/Plugins/UnityActions/Scripts/TestCases/TestActions.cs
@@ -43,7 +43,7 @@ public class TestActions : MonoBehaviour
 
 
 
-		StartCoroutine(anANimtion);
+		StartCoroutine(anANimtion());
 
 	}
 
